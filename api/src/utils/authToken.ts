cecba450e81ae77a5f50741ce764bb772fe467a9
Natalie <natalielunbeck@yahoo.com>
2024-04-30 15:51:29 -0700

    Revert "verify all tokens (test)"
    This reverts commit 59219afe4142b66fa43fcc74c3dc6a828a7115fa.

--- api/src/utils/authToken.ts
+++ api/src/utils/authToken.ts
@@ -1,17 +1,22 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
+import { isPlainObject } from 'lodash';
 
 import { InvalidTokenError } from 'errors';
 
 export const signToken = (payload: object, options?: SignOptions): string =>
   jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: '1000000 days',
+    expiresIn: '180 days',
     ...options,
   });
 
-export const verifyToken = (token: string): any => {
+export const verifyToken = (token: string): { [key: string]: any } => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    return payload;
+
+    if (isPlainObject(payload)) {
+      return payload as { [key: string]: any };
+    }
+    throw new Error();
   } catch (error) {
     throw new InvalidTokenError();
   }
